26e587724d2c72cea494a8502aea8c172642c9ae
jlx321@126.com <jlx321@126.com>
2022-12-17 12:17:30 +0800

    HandCard: Struct for two cards in hand

---
+++ Card/HandCard.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#include "PokerCard.h"
+
+namespace th
+{
+    struct HandCard
+    {
+        HandCard();
+
+        th::PokerCard firstHandCard;
+        th::PokerCard secondHandCard;
+    };
+
+
+}
