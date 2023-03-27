765890c6f6a748cc9babd6143e6600ff429573f1
jlx321@126.com <jlx321@126.com>
2023-03-27 20:44:26 +0800

    [#14] GameProcess: Rename to GameTable

--- Game/GameProcess.h
+++ Game/GameProcess.h
@@ -13,21 +13,6 @@ public:
     GameProcess();
     ~GameProcess() = default;
 
-    bool initPokerTable(const int32_t playerNum,
-                        const int32_t initChip);
-    void clearPokerTable();
-
-    void    startANewGame();
-    int32_t getCurrPool() const;
-    int32_t getGameRound() const;
-
 private:
-    const int32_t MINIMUM_PLAYER_NUM;
-    const int32_t MAXIMUM_PLAYER_NUM;
-
-    int32_t currPool;
-    int32_t gameRound;
-
-    std::vector<std::shared_ptr<th::BasePlayer>> players;
 };
 } // namespace th
