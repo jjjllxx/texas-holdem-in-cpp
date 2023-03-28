a0d6ccff45795bd2c7cefb00668851fc81b3cfa8
jlx321@126.com <jlx321@126.com>
2023-03-28 19:54:56 +0800

    [#14] Constants: Add STANDARD_CARD_COMBO_SIZE

--- Utilities/Constants.h
+++ Utilities/Constants.h
@@ -2,6 +2,8 @@
 
 namespace th
 {
+inline constexpr std::size_t STANDARD_CARD_COMBO_SIZE = 5;
+
 inline constexpr int32_t MINIMUM_PLAYER_NUM = 2;
 inline constexpr int32_t MAXIMUM_PLAYER_NUM = 12;
 } // namespace th
