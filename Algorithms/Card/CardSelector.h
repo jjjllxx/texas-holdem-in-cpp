70f502f57f4868582d64aaf3cc4903d8038310f0
jlx321@126.com <jlx321@126.com>
2023-09-02 14:09:41 +0800

    [#24] CardSelector: Use permutation instead of backtrack for readablility

--- Algorithms/Card/CardSelector.h
+++ Algorithms/Card/CardSelector.h
@@ -9,13 +9,5 @@ struct PokerCard;
 namespace CardSelector
 {
     std::vector<th::PokerCard> selectHighestCardCombo(const std::vector<th::PokerCard>& fromCards);
-
-    void selectFiveCards(const std::size_t                 pos,
-                         const std::vector<th::PokerCard>& fromCards,
-                         std::vector<th::PokerCard>&       currHighest,
-                         std::vector<th::PokerCard>&       currSelected);
-
-    std::vector<th::PokerCard> selectTheHigher(const std::vector<th::PokerCard>& firstCards,
-                                               const std::vector<th::PokerCard>& secondCards);
 } // namespace CardSelector
 } // namespace th
