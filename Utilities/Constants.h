bf5b201f2b7e32e9e987be751cc8e4d0b84a4d4d
jlx321@126.com <jlx321@126.com>
2023-09-02 14:16:03 +0800

    [#24] Constants: Add STANDARD_CARD_DECK_SIZE

--- Utilities/Constants.h
+++ Utilities/Constants.h
@@ -6,6 +6,7 @@
 namespace th
 {
 inline constexpr std::size_t STANDARD_CARD_COMBO_SIZE = 5;
+inline constexpr std::size_t STANDARD_CARD_DECK_SIZE  = 52;
 
 inline constexpr int32_t MINIMUM_PLAYER_NUM = 2;
 inline constexpr int32_t MAXIMUM_PLAYER_NUM = 12;
