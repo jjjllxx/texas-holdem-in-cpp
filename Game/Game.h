95364f20ff6025685fb7a1f59d3d73254cd31704
jlx321@126.com <jlx321@126.com>
2023-09-10 14:53:17 +0800

    [#28] Game: Generalise each round as oneRound() function

--- Game/Game.h
+++ Game/Game.h
@@ -3,6 +3,7 @@
 #include "Entity/Chip/Chip.h"
 
 #include <memory>
+#include <string>
 #include <unordered_map>
 #include <vector>
 
@@ -31,21 +32,16 @@ private:
     void handleBlinds(std::vector<std::shared_ptr<th::BasePlayer>>& players);
     void dealCards(th::CardDeck&                                 cardDeck,
                    std::vector<std::shared_ptr<th::BasePlayer>>& players);
-    void preflopRound(std::vector<std::shared_ptr<th::BasePlayer>>& players);
-    void flopRound(th::CardDeck&                                 cardDeck,
-                   std::vector<std::shared_ptr<th::BasePlayer>>& players);
-    void turnRound(th::CardDeck&                                 cardDeck,
-                   std::vector<std::shared_ptr<th::BasePlayer>>& players);
-    void riverRound(th::CardDeck&                                 cardDeck,
-                    std::vector<std::shared_ptr<th::BasePlayer>>& players);
-    void oneRound(const std::size_t                             startAt,
+    void oneRound(const std::size_t                             cardNumToReveal,
+                  const std::string&                            roundName,
+                  th::CardDeck&                                 cardDeck,
                   std::vector<std::shared_ptr<th::BasePlayer>>& players);
 
-    void revealOnePublicCard(th::CardDeck& cardDeck);
-    void collectChips(std::vector<std::shared_ptr<th::BasePlayer>>& players);
-    void addToPool(const th::chip& chip);
-
-    void showCurrPublicCards() const;
     void logGameStatus(const std::string& status) const;
+    void revealPublicCards(const std::size_t cardNumToReveal,
+                           th::CardDeck&     cardDeck);
+    void showCurrPublicCards() const;
+    void playersTakeAction(std::vector<std::shared_ptr<th::BasePlayer>>& players);
+    void collectChips(std::vector<std::shared_ptr<th::BasePlayer>>& players);
 };
 } // namespace th
