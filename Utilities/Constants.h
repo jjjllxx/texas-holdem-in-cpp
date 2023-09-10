3d14b7dade075f32f5a83ea9cb1ae33f3d904a1b
jlx321@126.com <jlx321@126.com>
2023-09-10 15:13:16 +0800

    [#28] Game: Deal cards from small blind position - To be consistent with the game rule

--- Utilities/Constants.h
+++ Utilities/Constants.h
@@ -2,11 +2,18 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <string>
 
 namespace th
 {
+inline constexpr std::size_t STANDARD_HAND_CARD_SIZE  = 2;
 inline constexpr std::size_t STANDARD_CARD_COMBO_SIZE = 5;
 inline constexpr std::size_t STANDARD_CARD_DECK_SIZE  = 52;
+
+inline const std::string PREFLOP_ROUND = "Preflop Round";
+inline const std::string FLOP_ROUND    = "Flop Round";
+inline const std::string TURN_ROUND    = "Turn Round";
+inline const std::string RIVER_ROUND   = "River Round";
 
 inline constexpr int32_t MINIMUM_PLAYER_NUM = 2;
 inline constexpr int32_t MAXIMUM_PLAYER_NUM = 12;
